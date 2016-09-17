aa450419bdb0969e292b2e122a52fa9e89dab7fa
luca3104 <s1210006@u-aizu.ac.jp>
2016-09-18 01:46:30 +0900

    create example project

---
+++ Example/Example/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Kaji Satoshi on 2016/09/18.
+//  Copyright © 2016年 Kaji Satoshi. All rights reserved.
+//
+
+import UIKit
+import IrohaSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        print(createSeed())
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
