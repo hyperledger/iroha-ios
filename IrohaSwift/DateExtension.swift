14f7ae6a5a7c421e9cec258cd5891bff4a57d7cb
luca3104 <s1210006@u-aizu.ac.jp>
2016-09-19 01:53:12 +0900

    create HttpRequest Model

---
+++ IrohaSwift/DateExtension.swift
@@ -0,0 +1,9 @@
+//
+//  DateEx.swift
+//  IrohaSwift
+//
+//  Created by Kaji Satoshi on 2016/09/19.
+//  Copyright © 2016年 Kaji Satoshi. All rights reserved.
+//
+
+import Foundation
---
+++ IrohaSwift/HttpRequest.swift
@@ -0,0 +1,72 @@
+//
+//  HttpRequest.swift
+//  IrohaSwift
+//
+//  Created by Kaji Satoshi on 2016/09/18.
+//  Copyright © 2016年 Kaji Satoshi. All rights reserved.
+//
+
+import Foundation
+
+class HttpRequest{
+    func getRequest(host:String, port:Int?, endpoint:String) ->[String:Any] {
+        var url : String
+        if(port != nil){
+            url = "\(host):\(port)\(endpoint)"
+        } else {
+            url = "\(host)\(endpoint)"
+        }
+        var request = URLRequest(url: URL(string:url)!)
+        request.httpMethod = "GET"
+        var d: [String:Any]? = nil
+
+        let semaphore = DispatchSemaphore(value: 0)
+        URLSession.shared.dataTask(with: request) {data, response, err in
+            if(data != nil){
+                let json = try! JSONSerialization.jsonObject(with: data!, options: [])
+                if let dictFromJSON = json as? [String:Any] {
+                    d = dictFromJSON
+                    semaphore.signal()
+                }
+            }else{
+                d = [
+                    "status": 404,
+                    "message": "not found"
+                ]
+                semaphore.signal()
+            }
+        }.resume()
+        semaphore.wait(timeout: .distantFuture)
+        return d!
+    }
+    
+    func postRequest(host:String, port:Int?, endpoint:String, parameters:[String:Any]?) ->[String:Any] {
+        var url : String
+        if(port != nil){
+            url = "\(host):\(port)\(endpoint)"
+        } else {
+            url = "\(host)\(endpoint)"
+        }
+        var request = URLRequest(url: URL(string:"https://faia9q1b3l.execute-api.ap-northeast-1.amazonaws.com/dev/getStoreList")!)
+        if(parameters != nil){
+            print(parameters!)
+            let jsonData = try! JSONSerialization.data(withJSONObject: parameters!, options: [])
+            request.httpBody = jsonData
+        }
+        request.httpMethod = "POST"
+        var d: [String:Any]? = nil
+        
+        let semaphore = DispatchSemaphore(value: 0)
+        URLSession.shared.dataTask(with: request) {data, response, err in
+            let json = try! JSONSerialization.jsonObject(with: data!, options: [])
+            print(data)
+            if let dictFromJSON = json as? [String:Any] {
+                d = dictFromJSON
+                semaphore.signal()
+            }
+            }.resume()
+        semaphore.wait(timeout: .distantFuture)
+        return d!
+    }
+
+}
