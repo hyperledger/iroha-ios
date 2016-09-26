0390d502a84aca9cc4ff60229980343203c75ee3
luca3104 <s1210006@u-aizu.ac.jp>
2016-09-27 01:30:22 +0900

    remove pod file in Example project ; conflict module map in framework editing

--- Example/Pods/Target Support Files/IrohaSwift/IrohaSwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_IrohaSwift : NSObject
-@end
-@implementation PodsDummy_IrohaSwift
-@end
--- Example/Pods/Target Support Files/IrohaSwift/IrohaSwift-umbrella.h
+++
@@ -1,7 +0,0 @@
-#import <UIKit/UIKit.h>
-
-#import "IrohaSwift.h"
-
-FOUNDATION_EXPORT double IrohaSwiftVersionNumber;
-FOUNDATION_EXPORT const unsigned char IrohaSwiftVersionString[];
-
