d9d63dd9fae7ec3e816070150b594cf6cf9bc680
luca3104 <s1210006@u-aizu.ac.jp>
2016-12-08 20:22:19 +0900

    init point demo app project

---
+++ Example-Point/Example-Point/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Example-Point
+//
+//  Created by Kaji Satoshi on 2016/12/08.
+//  Copyright © 2016年 Soramitsu Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
