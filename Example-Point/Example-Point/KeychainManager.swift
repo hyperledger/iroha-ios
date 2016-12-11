ddd58c6793e6866fbce3ab11a4f5abe16e6e81b0
luca3104 <s1210006@u-aizu.ac.jp>
2016-12-12 03:28:33 +0900

    add keychain managert

---
+++ Example-Point/Example-Point/KeychainManager.swift
@@ -0,0 +1,9 @@
+//
+//  KeychainManager.swift
+//  Example-Point
+//
+//  Created by Kaji Satoshi on 2016/12/12.
+//  Copyright © 2016年 Soramitsu Co., Ltd. All rights reserved.
+//
+
+import Foundation
