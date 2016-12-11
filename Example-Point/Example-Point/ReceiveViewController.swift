d1333f23e516dc27b36a3701541c0fd466985c2e
luca3104 <s1210006@u-aizu.ac.jp>
2016-12-12 03:16:11 +0900

    add view controllers

---
+++ Example-Point/Example-Point/ReceiveViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ReceiveViewController.swift
+//  Example-Point
+//
+//  Created by Kaji Satoshi on 2016/12/12.
+//  Copyright © 2016年 Soramitsu Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ReceiveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
