671b1de3e877511464efedb83345a2dcc41ddacd
luca3104 <s1210006@u-aizu.ac.jp>
2016-12-15 18:25:43 +0900

    add color extension for iroha color

---
+++ Example-Point/Example-Point/CheckReachablility.swift
@@ -0,0 +1,9 @@
+//
+//  CheckReachablility.swift
+//  Example-Point
+//
+//  Created by Kaji Satoshi on 2016/12/15.
+//  Copyright © 2016年 Soramitsu Co., Ltd. All rights reserved.
+//
+
+import Foundation
---
+++ Example-Point/Example-Point/ColorExtension.swift
@@ -0,0 +1,19 @@
+//
+//  ColorExtension.swift
+//  Example-Point
+//
+//  Created by Kaji Satoshi on 2016/12/15.
+//  Copyright © 2016年 Soramitsu Co., Ltd. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    class var iroha:UIColor {
+        get {
+            return UIColor(red: 228/255, green: 35/255, blue: 45/255, alpha: 1)
+        }
+    }
+
+}
