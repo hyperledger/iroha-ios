4bca1a9cf28041308d4830218f08995e654a124f
luca3104 <s1210006@u-aizu.ac.jp>
2016-12-15 18:27:04 +0900

    add checkreachability function

--- Example-Point/Example-Point/CheckReachablility.swift
+++ Example-Point/Example-Point/CheckReachablility.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+import SystemConfiguration
+
+func CheckReachability(host_name:String)->Bool{
+    
+    let reachability = SCNetworkReachabilityCreateWithName(nil, host_name)!
+    var flags = SCNetworkReachabilityFlags.connectionAutomatic
+    if !SCNetworkReachabilityGetFlags(reachability, &flags) {
+        return false
+    }
+    let isReachable = (flags.rawValue & UInt32(kSCNetworkFlagsReachable)) != 0
+    let needsConnection = (flags.rawValue & UInt32(kSCNetworkFlagsConnectionRequired)) != 0
+    return (isReachable && !needsConnection)
+}
