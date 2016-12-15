3f2992c51c68685b216634ccae28426fea8b61eb
luca3104 <s1210006@u-aizu.ac.jp>
2016-12-15 18:26:22 +0900

    add transaction cell view

--- Example-Point/Example-Point/TransactionCEll.swift
+++
@@ -1,9 +0,0 @@
-//
-//  TransactionCEll.swift
-//  Example-Point
-//
-//  Created by Kaji Satoshi on 2016/12/15.
-//  Copyright © 2016年 Soramitsu Co., Ltd. All rights reserved.
-//
-
-import Foundation
