c984cd13adcea2b77b9d2eb30dc0bb402bc8324b
Russel <emkil.russel@gmail.com>
2019-01-19 15:45:42 +0300

    new library implementation add
    Signed-off-by: Russel <emkil.russel@gmail.com>

---
+++ IrohaCommunication/Classes/Private/Queries/IRQueryAll.h
@@ -0,0 +1,16 @@
+#ifndef IRQueryAll_h
+#define IRQueryAll_h
+
+#import "IRGetAccount.h"
+#import "IRGetSignatories.h"
+#import "IRGetAccountTransactions.h"
+#import "IRGetAccountAssetTransactions.h"
+#import "IRGetTransactions.h"
+#import "IRGetAccountAssets.h"
+#import "IRGetAccountDetail.h"
+#import "IRGetRoles.h"
+#import "IRGetRolePermissions.h"
+#import "IRGetAssetInfo.h"
+#import "IRGetPendingTransactions.h"
+
+#endif /* IRQueryAll_h */
